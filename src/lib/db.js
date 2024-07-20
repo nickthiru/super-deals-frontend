f1439ca3a8e3e96b6841cc2e8c1decd21f0d0971
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-07-20 15:30:42 +0530

    First draft of dexie DB.

---
+++ src/lib/db.js
@@ -0,0 +1,21 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+import Dexie from "dexie";
+
+export const db = new Dexie("appDb");
+
+db.version(1).stores({
+  product: `
+    id,
+    name,
+    category,
+    price,
+    ratings
+  `
+});
+
+db.open().then(function (db) {
+  console.log("Database opened successfully");
+}).catch(function (err) {
+  console.error(err);
+});
