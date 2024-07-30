55916e0ef266ee904d6c6e723148442401d84dc6
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-07-30 14:39:06 +0530

    Clean-up.

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Button from "$lib/components/Button.svelte";
+  // import Button from "$lib/components/Button.svelte";
 </script>
 
 
 <h1>Welcome to the Landing Page</h1>
 
-<Button>Test</Button>
-<Button style="outline">Testing</Button>
+<!-- <Button>Test</Button>
+<Button style="outline">Testing</Button> -->
