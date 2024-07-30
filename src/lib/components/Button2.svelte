089ac0f8593c3c2fd8d2f3553c82cd19c41ae8ff
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-07-30 14:12:45 +0530

    Testing customizable components. Reorganized CSS files.

---
+++ src/lib/components/Button2.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let style = "";
+  const classes = "base " + style;
+</script>
+
+
+<button class={classes}>
+  <slot />
+</button>
+
+
+<style>
+  .base {
+    border-radius: 3px;
+    display: inline-block;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 600;
+    padding: 13px 40px;
+    border: 0;
+    transition: background-color 200ms, color 200ms;
+  }
+
+  .outline {
+    box-shadow: 1px 1px 7px #c4c4c4;
+    padding: 8px 25px;
+    border-radius: 10px;
+  }
+</style>
--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+  // import Button from "$lib/components/Button.svelte";
+</script>
+
+
 <h1>Welcome to the Landing Page</h1>
+
+<!-- <Button style="outline">Testing</Button>
+<Button>Test</Button> -->
