ee39524fea07bb16665385207bb820c1c6aa7282
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-07-30 15:12:33 +0530

    Addded Container component.

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,11 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    max-width: 960px;
+    padding-inline: 20px;
+    margin-inline: auto;
+  }
+</style>
--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,9 +1,11 @@
 <script>
+  import Container from "$lib/components/Container.svelte";
   // import Button from "$lib/components/Button.svelte";
 </script>
 
-
-<h1>Welcome to the Landing Page</h1>
+<Container>
+  <h1>Welcome to the Landing Page</h1>
+</Container>
 
 <!-- <Button>Test</Button>
 <Button style="outline">Testing</Button> -->
