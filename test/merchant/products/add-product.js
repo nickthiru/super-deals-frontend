9736e824cc14adbb1033513658d8019a7459063f
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-06 00:03:09 +0530

    Additional samle data idea.

--- test/merchant/products/add-product.js
+++ test/merchant/products/add-product.js
@@ -23,6 +23,40 @@ Data:
     dateAdded: Date
   }
 
+  {
+    "Id": 123,
+    "Title": "Bicycle 123",
+    "Description": "123 description",
+    "BicycleType": "Hybrid",
+    "Brand": "Brand-Company C",
+    "Price": 500,
+    "Color": ["Red", "Black"],
+    "ProductCategory": "Bicycle",
+    "InStock": true,
+    "QuantityOnHand": null,
+    "RelatedItems": [
+        341,
+        472,
+        649
+    ],
+    "Pictures": {
+        "FrontView": "http://example.com/products/123_front.jpg",
+        "RearView": "http://example.com/products/123_rear.jpg",
+        "SideView": "http://example.com/products/123_left_side.jpg"
+    },
+    "ProductReviews": {
+      "FiveStar": [
+          "Excellent! Can't recommend it highly enough! Buy it!",
+          "Do yourself a favor and buy this."
+      ],
+      "OneStar": [
+          "Terrible product! Do not buy this."
+      ]
+    },
+    "Comment": "This product sells out quickly during the summer",
+    "Safety.Warning": "Always wear a helmet"
+  }
+
 Infra:
 
   API
