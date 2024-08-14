6ca292bda434ff7da2adf77b531aa75687cf590c
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-14 14:44:30 +0530

    Added layout for auth page.

---
+++ src/routes/(auth)/auth/+layout.svelte
@@ -0,0 +1,13 @@
+<!-- <script>
+  import { page } from "$app/stores";
+
+  let url = $page.route.id?.split("/")
+  console.log("route: " + url);
+
+  let route = url?.[url?.length - 1];
+  console.log("route: " + route);
+</script>
+
+<h1>{route?.[0].toUpperCase() + route.slice(1)}</h1> -->
+
+<slot />
