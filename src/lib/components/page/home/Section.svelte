8caecbb2f4a86b4e1158e36886ced72324333b15
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-19 00:16:36 +0530

    "Section" component for "Home Page" layout.

---
+++ src/lib/components/page/home/Section.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let section = "";
+</script>
+
+<section 
+  aria-labelledby={section} 
+  style:grid-area={section}
+>
+  <slot />
+</section>
+
+
+<style>
+  section {
+    border: 1px solid black;
+    position: relative;
+  }
+</style>
