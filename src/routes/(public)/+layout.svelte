be9e0369319ee3702febfc8d1194daf54b954f14
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-18 20:40:23 +0530

    Moved "NavBar" component code directly to layout.

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,8 +1,35 @@
 <script>
-  import NavBar from "$lib/components/common/NavBar.svelte";
+  import Link from "$lib/components/shared/Link.svelte";
+  import Text from "$lib/components/shared/Text.svelte";
 </script>
 
 
 <slot />
 
-<NavBar />
+<nav>
+  <Link type="nav">
+    <Text type="sm">Home</Text>
+  </Link>
+  <Link type="nav">
+    <Text type="sm">Brands</Text>
+  </Link>
+  <Link type="nav">
+    <Text type="sm">Editor's Choice</Text>
+  </Link>
+  <Link type="nav">
+    <Text type="sm">Settings</Text>
+  </Link>
+</nav>
+
+
+<style>
+  nav {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    position: fixed;
+    bottom: 0;
+
+    height: 50px;
+    border: 1px solid black;
+  }
+</style>
