93b22ebed3bfdcdb33e3a42c7e172547e238bedd
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-20 03:10:35 +0530

    "Home" page WIP.

--- src/lib/components/page/home/categories/CategoriesItem.svelte
+++ src/lib/components/page/home/categories/CategoriesItem.svelte
@@ -1,21 +1,26 @@
 <script>
+  import Link from "$lib/components/shared/Link.svelte";
   import Text from "$lib/components/shared/Text.svelte";
   import "iconify-icon";
 
   export let category;
 </script>
 
-
-<div class="categories-item" aria-labelledby={category.name}>
-  <div class="categories-item__icon">
-    <iconify-icon icon={category.icon} />
+<Link url="#">
+  <div 
+  class="categories-item" 
+  aria-labelledby={category.name}
+  >
+    <div class="categories-item__icon">
+      <iconify-icon icon={category.icon} />
+    </div>
+    <div class="categories-item__text">
+      <Text id={category.name} class="sm">
+        {category.name}
+      </Text>
+    </div>
   </div>
-  <div class="categories-item__text">
-    <Text id={category.name} class="sm">
-      {category.name}
-    </Text>
-  </div>
-</div>
+</Link>
 
 
 <style>
