f5b5624c8aae045c38714555243ddd4fbbfee34c
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-21 14:30:01 +0530

    Moved "categories" data, which is used to load category names and their icons on the home page, to JS module that exports it as an object.

--- src/routes/(public)/+page.js
+++ src/routes/(public)/+page.js
@@ -1,4 +1,5 @@
 import { error } from '@sveltejs/kit';
+import { categories } from '$lib/data/categories';
 
 // /** @type {import('./$types').PageLoad} */
 // export function load({ params }) {
@@ -16,39 +17,6 @@ export function load({ params }) {
   console.log("Inside 'public/+page.js load()");
 
   return {
-    categories: [
-      {
-        icon: "ion:fast-food-outline",
-        name: "Food & Drink",
-      },
-      {
-        icon: "cil:bathroom",
-        name: "Bathroom",
-      },
-      {
-        icon: "map:jewelry-store",
-        name: "Jewelry",
-      },
-      {
-        icon: "material-symbols:sports-tennis-outline",
-        name: "Sports",
-      },
-      {
-        icon: "zondicons:computer-laptop",
-        name: "Tech",
-      },
-      {
-        icon: "clarity:car-line",
-        name: "Auto",
-      },
-      {
-        icon: "icon-park-outline:entertainment",
-        name: "Entertainment",
-      },
-      {
-        icon: "streamline:travel-places-beach-island-waves-outdoor-recreation-tree-beach-palm-wave-water",
-        name: "Travel",
-      },
-    ]
+    categories,
   }
 }
