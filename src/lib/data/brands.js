972c15b610d26f4fa7ec6b5ec92a34139cedac03
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-26 09:15:36 +0530

    Some sample data.

---
+++ src/lib/data/brands.js
@@ -0,0 +1,14 @@
+export const brands = [
+  {
+    img: "img",
+    name: "Advance Auto Parts",
+  },
+  {
+    img: "img",
+    name: "Albertson's",
+  },
+  {
+    img: "img",
+    name: "Amazon",
+  },
+]
---
+++ src/lib/data/deals.js
@@ -0,0 +1,62 @@
+export const deals = [
+  {
+    title: "Samsung 65\" QLED 4K Smart TV - 2023 Model",
+    price: "$899.99",
+    originalPrice: "$1299.99",
+    discount: "31%",
+    link: "https://slickdeals.net/f/15102518-samsung-65-qled-4k-smart-tv-899-99",
+    brand: "Best Buy",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/29/Best_Buy_Logo.svg",
+    rating: 4.7,
+    category: "Electronics",
+    expiration: "2024-08-30"
+  },
+  {
+    title: "Apple AirPods Pro (2nd Gen)",
+    price: "$179.99",
+    originalPrice: "$249.99",
+    discount: "28%",
+    link: "https://slickdeals.net/f/15102519-apple-airpods-pro-2nd-gen-179-99",
+    brand: "Amazon",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    rating: 4.8,
+    category: "Tech",
+    expiration: "2024-09-01"
+  },
+  {
+    title: "Ninja Air Fryer - 4 Quart",
+    price: "$89.99",
+    originalPrice: "$119.99",
+    discount: "25%",
+    link: "https://slickdeals.net/f/15102520-ninja-air-fryer-4-quart-89-99",
+    brand: "Walmart",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Walmart_logo.svg",
+    rating: 4.5,
+    category: "Home Appliances",
+    expiration: "2024-08-29"
+  },
+  {
+    title: "Nike Men's Running Shoes",
+    price: "$59.99",
+    originalPrice: "$90.00",
+    discount: "33%",
+    link: "https://slickdeals.net/f/15102521-nike-men-s-running-shoes-59-99",
+    brand: "Nike",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg",
+    rating: 4.6,
+    category: "Footwear",
+    expiration: "2024-08-31"
+  },
+  {
+    title: "Dell Inspiron 15 3000 Laptop",
+    price: "$399.99",
+    originalPrice: "$599.99",
+    discount: "33%",
+    link: "https://slickdeals.net/f/15102522-dell-inspiron-15-3000-laptop-399-99",
+    brand: "Dell",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/4/48/Dell_Logo.svg",
+    rating: 4.4,
+    category: "Computers",
+    expiration: "2024-09-05"
+  }
+];
