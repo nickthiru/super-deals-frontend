1c91fba3eb828d7d6f994952a7b36a6ef1c57d72
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-08-26 09:13:13 +0530

    Created different "Card" component variants.

---
+++ src/lib/components/shared/card/CardBrandsList.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Text from "$lib/components/shared/Text.svelte";
+
+  export let brand;
+</script>
+
+<div>
+  <p>{brand.img}</p>
+  <!-- <p>{brand.name}</p> -->
+  <Text>{brand.name}</Text>
+</div>
+
+
+<style>
+  div {
+    display: flex;
+  }
+</style>
