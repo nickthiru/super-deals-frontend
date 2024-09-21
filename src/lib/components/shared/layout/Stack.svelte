1704bbd379149c3f656dd076dd4cabb58a56100b
Nick Thiru <nickthiru@gmail.com>
2024-09-22 01:08:00 +0530

    WIP on components/layout

---
+++ src/lib/components/shared/layout/Stack.svelte
@@ -0,0 +1,20 @@
+<div class="stack">
+  <slot />
+</div>
+
+
+<style>
+  .stack {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  :global(.stack > *) {
+    margin-block: 0;
+  }
+
+  :global(.stack > * + *) {
+    margin-block-start: var(--space, 1.5rem);
+  }
+</style>
