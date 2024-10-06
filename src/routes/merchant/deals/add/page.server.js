4cfc1d95987d0ee154d71a4bb2e208aafb0d51ec
Nick Thiru <nickthiru@gmail.com>
2024-10-06 19:33:52 +0530

    WIP on add-deal

--- src/routes/merchant/deals/add/page.server.js
+++ src/routes/merchant/deals/add/page.server.js
@@ -10,17 +10,17 @@ export const actions = {
 
     const formData = await request.formData();
 
-    // const merchantId = formData.get("merchantId");
-    // console.log("merchantId: " + merchantId);
+    const merchantId = formData.get("merchantId");
+    console.log("merchantId: " + merchantId);
 
     const title = formData.get("title");
     console.log("title: " + title);
 
-    // const originalPrice = formData.get("originalPrice");
-    // console.log("originalPrice: " + originalPrice);
+    const originalPrice = formData.get("originalPrice");
+    console.log("originalPrice: " + originalPrice);
 
-    // const discount = formData.get("discount");
-    // console.log("discount: " + discount);
+    const discount = formData.get("discount");
+    console.log("discount: " + discount);
 
     // const logoFileInBase64 = formData.get("logoFileInBase64");
     // console.log("logoFileInBase64: " + logoFileInBase64);
@@ -28,31 +28,34 @@ export const actions = {
     // console.log("logoFileInBase64 is empty: " + R.isEmpty(logoFileInBase64));
 
     const logo = formData.get("logo");
-    // console.log("logo: " + logo);
-    // console.log('typeof logo: ' + typeof logo);
-    // console.log("logo is empty: " + R.isEmpty(logo));
+    console.log("logo: " + logo);
+    console.log('typeof logo: ' + typeof logo);
+    console.log("logo is empty: " + R.isEmpty(logo));
 
-    // const category = formData.get("category");
-    // console.log("category: " + category);
+    const category = formData.get("category");
+    console.log("category: " + category);
 
     // const expiration = formData.get("expiration");
     // console.log("expiration: " + expiration);
 
-    // const deal = {
-    //   merchantId,
-    //   title,
-    //   originalPrice,
-    //   discount,
-    //   logo,
-    //   logoFileInBase64,
-    //   category,
-    //   expiration,
-    // };
+    const deal = {
+      merchantId,
+      title,
+      originalPrice,
+      discount,
+      logo,
+      // logoFileInBase64,
+      category,
+      // expiration,
+    };
 
-    const response = await fetch("https://mms6a4j564.execute-api.us-east-1.amazonaws.com/prod/merchant/deals", {
-      method: "POST",
-      body: formData,
-    });
+    // const response = await fetch("https://mms6a4j564.execute-api.us-east-1.amazonaws.com/prod/merchant/deals", {
+    //   method: "POST",
+    //   body: JSON.stringify(deal),
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   }
+    // });
 
     // const result = await response.json();
     // console.log("result: ", JSON.stringify(result, null, 2));
